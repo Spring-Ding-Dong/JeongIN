a50d83e4f000fceb2e38200374c6897b0f6bdf47
jeongIN <shjin4667@gmail.com>
2025-01-14 21:05:13 +0900

    Initial commit

---
+++ src/main/java/com/springDingDong/jeongin/JeonginApplication.java
@@ -0,0 +1,13 @@
+package com.springDingDong.jeongin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeonginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeonginApplication.class, args);
+	}
+
+}
